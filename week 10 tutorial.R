4e38fbbda637d6429d48c358c099b55ab90ca2bc
mbawa3neh <mbawaneh.1978@gmail.com>
2024-10-23 18:04:00 +1100

    Initial commit: Add CSV data and R tutorial files

---
+++ week 10 tutorial.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+
+data = read.csv("D:/Western Sydney/2024/Spring/COMP7024 (Spring 2024) Programming for Data Science/Week 10/epiSEIHCRD_combAge.csv")
+
+# We find that the time variable is not of type Date
+# So it is changed to date type where time 0 is 1/3/2020
+data$t = as.Date(data$t, origin = "2020-03-01")
+
+# The default palette is not suitable for those with colour blindness
+# Set the colour palette for plots
+
+cbbPalette = c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442",
+                "#0072B2", "#D55E00", "#CC79A7")
+palette(cbbPalette)
+
+# We get the required date interval:
+
+dDate = subset(data, subset = ((t >= "2020-06-01") & (t <= "2020-12-31")))
+
+g = ggplot(data = dDate) +
+  geom_line(mapping = aes(x = t, y = H))
+print(g)
